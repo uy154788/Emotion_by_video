b64ee37d1470f7fb893a0b47b5915e8c3e62694f
uy154788 <118651983+uy154788@users.noreply.github.com>
2024-09-12 00:57:55 +0530

    1209

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import cv2
+import os
 from deepface import DeepFace
 
 app = Flask(__name__)
@@ -78,4 +79,6 @@ def analyze_video():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
