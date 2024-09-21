bb6a071ae8d08eb5f55932f3b7e9b22396b7cabf
uy154788 <118651983+uy154788@users.noreply.github.com>
2024-09-22 00:42:24 +0530

    22-09

--- app.py
+++ app.py
@@ -79,6 +79,4 @@ def analyze_video():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(debug=True)
